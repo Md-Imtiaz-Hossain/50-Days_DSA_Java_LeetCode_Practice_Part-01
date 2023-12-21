a7fcc96198ebdaa17d7426924a6278318bcf9c1f
Md Imtiaz Hossain <imtiaz00513@gmail.com>
2023-12-21 14:43:43 +0600

    Working on day 05

---
+++ src/main/java/com/iimtiaz/day_05/_4Sum.java
@@ -0,0 +1,21 @@
+package com.iimtiaz.day_05;
+
+import java.util.List;
+
+public class _4Sum {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        System.out.println(new Solution().fourSum(nums, target));
+        System.out.println();
+
+    }
+}
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        return null;
+    }
+}

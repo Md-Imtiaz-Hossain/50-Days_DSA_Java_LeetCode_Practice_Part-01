999687d215ffafc8f902158eb072cf5863e8fc0b
Md Imtiaz Hossain <imtiaz005123@gmail.com>
2023-12-22 23:10:52 +0600

    Day 05

--- src/main/java/com/iimtiaz/day_05/_4Sum.java
+++ src/main/java/com/iimtiaz/day_05/_4Sum.java
@@ -1,5 +1,6 @@
 package com.iimtiaz.day_05;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class _4Sum {
@@ -13,9 +14,45 @@ public class _4Sum {
     }
 }
 
+/**
+ * Time Complexity: The time complexity of the code is O(n3), where n
+ * is the length of the input array. This is because the code uses
+
+ * Space Complexity: The space complexity of the code is O(n), where n
+ * is the length of the input array. This is due to the additional space
+ * required for the set and list data structures. In the worst case, the set
+ * and list can store all elements of the array, hence the space complexity is linear.
+ */
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
-        return null;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = j + 1;
+                int l = nums.length - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        s.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+        output.addAll(s);
+        return output;
     }
 }
+
+//https://leetcode.com/problems/4sum/
